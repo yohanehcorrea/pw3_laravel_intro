394f2a5e15258718fb023588c7da249a3ce10ec5
Yohane Correa <yohaneheleonoracorrea@gmail.com>
2026-08-11 13:49:54 -0300

    wip: Landing Page com Tailwind

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::view('/landing' , 'landing');
+
 Route::get('/produtos', [ProdutoController::class , 'index']);
 Route::post('/produtos', [ProdutoController::class , 'store']);
 
