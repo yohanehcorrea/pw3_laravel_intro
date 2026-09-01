cd43a0d9e2be2f9d368c063f64bf4609f5dff370
Yohane Correa <yohaneheleonoracorrea@gmail.com>
2026-09-01 14:00:27 -0300

    wip: CRUD create User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Exibe o formulario de cadastro de usuarios
+     */
+    public function create()
+    {
+        return view('users.create');
+    
+    }
+    /**
+     * Salvar o novo usuariono banco de dados com validaçao
+     */
+    public function store(Request $request)
+    {
+        //Validação dos campos de formulario
+
+        $dadosValidados = $request->validate([
+        'name' => 'required|min:3|max:255',
+        'email' => 'required|email|unique:user, email',
+        'password' => 'required|min:6'
+        ]);
+
+        //Persistencia no banco de dados usando o ORM Eloquent
+         User::create($dadosValidados);
+
+        //Redirecionar para o o painel administrativo com mensagem de sucesso
+        return redirect('/admin')->with('sucesso','Usuario cadastrado com sucesso');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     </div>
 </header>
 
-    <main>
+    <main class="container mx-auto my-8 px-4">
         @yield('content')
     </main>
 
